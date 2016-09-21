abff5e5d90f71f99f21967d850d32473b3ed3f22
Jordan Li <jordanwyli@gmail.com>
2016-09-20 21:07:12 -0700

    hw1 Jordan Li

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    a = a.map { |e| Integer(e)+2 }
+    a = a.reject { |e| e > 10 || e % 2 != 0 }
+    a = a.uniq
+
+    return a.inject(0){|sum,x| sum + x }
   end
 end
-
-
